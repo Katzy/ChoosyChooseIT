6138f5e51160c213123636302013df7207867a1f
Scott <scottalankatz@gmail.com>
2015-04-24 12:15:08 -0400

    Added registration test

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -4,13 +4,14 @@ require 'rails_helper'
 RSpec.feature "Registration" do
   scenario "User signs up, logs out, logs back in" do
     visit "/"
-    click_link_or_button "Sign up"
+    click_link_or_button "Sign_up"
     fill_in "Email", with: "katz@example.com"
     fill_in "Password", with: "password"
-    fill_in "Password Confirmation", with: "password"
-    click_link_or_button "Submit"
+    fill_in "Password confirmation", with: "password"
+    click_link_or_button "Sign up"
 
+    expect(User.find_by(email: "katz@example.com")).to be_present
 
-    expext(page).to have_content "Welome katz@example.com"
+    expect(page).to have_content "Welcome! You have signed up successfully."
   end
 end
