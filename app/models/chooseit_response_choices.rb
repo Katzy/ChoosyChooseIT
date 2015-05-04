ff512b785f7600d707fd2bf0590d83a63b0f2338
Scott <scottalankatz@gmail.com>
2015-05-04 19:06:38 -0400

    fixed associations in chooseit & chooseit_response_choices models

--- app/models/chooseit_response_choices.rb
+++ app/models/chooseit_response_choices.rb
@@ -1,7 +1,7 @@
 class ChooseitResponseChoices < ActiveRecord::Base
 
-  belongs_to :chooseit, :through => :chooseit_choice
+  belongs_to :chooseit, through: => :chooseit_choice
   belongs_to :chooseit_responses
-  belongs_to :user, :through => :chooseit_response
+  belongs_to :user, through: => :chooseit_response
 
 end
