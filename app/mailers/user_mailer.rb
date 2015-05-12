af89a6a7eea8386c40541115686b894585618870
Scott <scottalankatz@gmail.com>
2015-05-11 23:09:19 -0400

    Adjusted sign in and sign up views

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,7 +7,7 @@ class UserMailer < ActionMailer::Base
     @emails = @chooseit.emails.split(',')
     @chooseit_choice_1 = chooseit_choice_1
     @chooseit_choice_2 = chooseit_choice_2
-    mail(to: @emails, subject: "#{@user.name} needs your opinion on something.  Help them ChooseIT!")
+    mail(to: @emails, subject: "#{@user.name} needs your opinion on something.  You've been summoned to ChooseIT!")
   end
 
 
